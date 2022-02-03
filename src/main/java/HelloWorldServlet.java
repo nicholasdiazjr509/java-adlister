a200554d5875c224941b61b7de770e0a8120e9f1
Nicholas Diaz <nicholasdiazjr509@gmail.com>
2022-02-03 10:56:01 -0600

    serlvet-exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -2,14 +2,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import static java.lang.System.out;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+//       String title = "Hello World";
 
+        String name = req.getParameter("name");
+        PrintWriter writer = res.getWriter();
+//        writer.println("<h1>" +title+"</h1>");
 
-        response.getWriter().println("<h1>Hello, World!</h1>");
+        if (name == null) {
+            out.println("Hello World");
+
+        } else {
+            out.println("<h1>" + name + "</h1>");
+//problems with this
+        }
     }
 }
