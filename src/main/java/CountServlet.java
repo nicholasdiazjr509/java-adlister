4c9bd9e43e4095dea8277730522ae47603a48b68
Nicholas Diaz <nicholasdiazjr509@gmail.com>
2022-02-03 15:28:36 -0600

    servlet exercises

---
+++ src/main/java/CountServlet.java
@@ -0,0 +1,27 @@
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CountServlet extends HttpServlet {
+    static int counter = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        //    out.println("<h1>The count is: " + counter++ +".");
+
+        String reset = req.getParameter("reset");
+        if (reset == null) {
+            out.println("<h1>The count is: " + counter++ + ".");
+        } else {
+            counter = 0;
+            out.println("<h1>The count is: " + counter + ".");
+        }
+    }
+}
