eaa20a01ae30fd6702c8c6f82e69b4a897031bff
Nicholas Diaz <nicholasdiazjr509@gmail.com>
2022-02-07 10:01:40 -0600

    started beans exc

---
+++ src/main/java/Album.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class Album implements Serializable {
+    private long empNo;
+    private String firstName;
+    private String lastName;
+    private char gender;
+    private Date birthDate;
+    private Date hireDate;
+
+    //our constructors
+    public Album() {
+    }
+
+
+
+}
+//DESIGN the database
