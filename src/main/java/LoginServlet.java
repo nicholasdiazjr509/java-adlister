470f006b8a314aa1392f4f4ae30c8af4ce77ae20
Nicholas Diaz Jr <91964728+nicholasdiazjr509@users.noreply.github.com>
2022-02-08 13:48:09 -0600

    Update LoginServlet.java

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,32 +1,56 @@
 
+import javax.servlet.http.Cookie;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+    private final String username = "user";
+    private final String password = "password";
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException{
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        if(request.getSession().getAttribute("user") != null){
+            response.sendRedirect("/profile");
+        }
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException{
-            if(request.getMethod().equalsIgnoreCase("post")){
+//            if(request.getMethod().equalsIgnoreCase("post")){
                 String username = request.getParameter("username");
                 String password = request.getParameter("password");
-                if(username.equals("admin") && password.equals("password")){
+
+//                User user = DaoFactory.findByUsername(username);
+
+                if(this.username.equals(username) && this.password.equals(password)){
+                    HttpSession oldSession = request.getSession(false);
+                    if(oldSession != null){
+                        oldSession.invalidate();
+                    }
+                  HttpSession newSession = request.getSession(true);
+                    newSession.setMaxInactiveInterval(60);
+                    //session expiry 5 minutos
+
+                    Cookie message = new Cookie("message", "Welcome");
+                    response.addCookie(message);
                     response.sendRedirect("/profile");
+
                 }else{
+                    RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/login");
+                    PrintWriter out = response.getWriter();
+                    System.out.println("<font color=red> Either username or password is wrong.</font>");
                     response.sendRedirect("/login");
                 }
             }
 
     }
-}
+
